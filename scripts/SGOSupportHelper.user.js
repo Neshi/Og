6bf6a91ecb09b830861715f5ef539338268c7693
Dawid Jachnik <djachnik@reference.be>
2021-11-11 21:13:09 +0100

    init

---
+++ scripts/SGOSupportHelper.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         SGO+ Support Helper
+// @namespace    https://comastuff.com/
+// @version      0.1
+// @description  Skrypt ten przypisuje przy temacie nick operatora/ów bazując na informacjach jakie są umieszczone na stronie https://ogamepl.comastuff.com/ . Przy nieobsadzonych uniach nick nie jest dodawany.
+// @author       Neshi
+// @match        https://coma.gameforge.com/ticket/index.php?page=tickets&action=go
+// @match        https://ogamepl.comastuff.com/
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @icon         https://www.google.com/s2/favicons?domain=gameforge.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let uniSettings = [];
+
+    if (window.location.hostname == 'ogamepl.comastuff.com'){
+        let regex68 = /Universum 68 : (.*)/
+        let regex = /^(\(([^)]+)\).*:(.*))/
+
+        var lines = $('h3:contains("Operatorzy")').next().text().split('\n')
+
+        for(let i=0;i<lines.length;i++){
+            let line = lines[i].replace(' ','');
+            if (regex68.test(line)){
+                let match = line.match(regex68)
+                //console.log('key:68;value:'+match[1].replace(' ',''))
+                uniSettings.push({
+                    key:'68',
+                    value:match[1].replace(/\s/g,'')
+                });
+            }
+            if (regex.test(line)){
+                let match = line.match(regex)
+                //console.log('key:'+match[2]+';value:'+match[3].replace(' ',''))
+                uniSettings.push({
+                    key:match[2],
+                    value:match[3].replace(/\s/g,'')
+                });
+            }
+        }
+        GM_deleteValue('uniSettings');
+        GM_setValue('uniSettings',uniSettings);
+    }
+
+    if (window.location.hostname == 'coma.gameforge.com'){
+        uniSettings = GM_getValue('uniSettings');
+        if (uniSettings == undefined){
+            $('body').append('<a target=\'_blank\' href=\'https://ogamepl.comastuff.com/\'>Przejdź na stronę Teamu, aby załadować operatorów.</a> Następnie odśwież tą stronę.')
+        }
+        //console.log(uniSettings);
+
+        $('table table table tr:gt(2)').each(function(){
+            let currentServerText = $(this).find('td:eq(2)').text();
+            let go = uniSettings.find(x=>x.key==currentServerText).value;
+            if (go.length>0){
+                $(this).find('td:eq(1)').append(' ('+go+')')
+            }
+        });
+    }
+})();
