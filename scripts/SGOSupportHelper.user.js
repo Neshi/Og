0955602b56d08884d782ab2c8664b786e78ccae9
Dawid Jachnik <djachnik@reference.be>
2021-12-09 08:46:40 +0100

    added functionality that make the api codes more readable

--- scripts/SGOSupportHelper.user.js
+++ scripts/SGOSupportHelper.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         SGO+ Support Helper
 // @namespace    https://comastuff.com/
-// @version      0.5
-// @description  Skrypt ten przypisuje przy temacie nick operatora/ów bazując na informacjach jakie są umieszczone na stronie https://ogamepl.comastuff.com/ . Przy nieobsadzonych uniach nick nie jest dodawany.
+// @version      0.6
+// @description  Skrypt ten przypisuje przy temacie nick operatora/ów bazując na informacjach jakie są umieszczone na stronie https://ogamepl.comastuff.com/ . Przy nieobsadzonych uniach nick nie jest dodawany. Dodaje również link przy kodach API w zgłoszeniach.
 // @author       Neshi
 // @match        https://coma.gameforge.com/ticket/index.php?page=tickets*
+// @match        https://coma.gameforge.com/ticket/index.php?page=answer*
 // @match        https://ogamepl.comastuff.com/
 // @updateURL    https://github.com/Neshi/Og/raw/main/scripts/SGOSupportHelper.user.js
 // @downloadURL  https://github.com/Neshi/Og/raw/main/scripts/SGOSupportHelper.user.js
@@ -52,20 +53,50 @@
     }
 
     if (window.location.hostname == 'coma.gameforge.com'){
-        uniSettings = GM_getValue('uniSettings');
-        if (uniSettings == undefined){
-            $('body').append('<a target=\'_blank\' href=\'https://ogamepl.comastuff.com/\'>Przejdź na stronę Teamu, aby załadować operatorów.</a> Następnie odśwież tą stronę.')
-        }
-        //console.log(uniSettings);
+        if (window.location.search.indexOf('page=answer')>0){
+            let reportRegex = /[cs]r-pl-\d{2,3}-[^.*]{40}/g;
 
-        $('table table table tr:gt(2)').each(function(){
-            let currentServerText = $(this).find('td:eq(2)').text();
-            if (currentServerText != '00'){
-                let go = uniSettings.find(x=>x.key==currentServerText).value;
-                if (go.length>0){
-                    $(this).find('td:eq(1)').append(' ('+go+')')
+            jQuery('table.nav div').each(function(){ 
+                var match;
+                var tdHtml = jQuery(this).html();
+                var matches = [];
+                if (reportRegex.test(tdHtml)){
+                    match = tdHtml.match(reportRegex);
+                    if (match){
+                        matches.push(match[1]);
+                        console.log(match[1]);
+                    }
+                    while (match = reportRegex.exec(tdHtml)){
+                        matches.push(match[0]);
+                    }
+                    matches = matches.filter(function(item, pos) {
+                        return matches.indexOf(item) == pos;
+                    });
+                    for(var i=0;i<matches.length;i++){
+                        tdHtml = tdHtml.replace(matches[i],matches[i]+'&nbsp;<a href="https://nomoreangel.de/api-reader/?apiid='+matches[i]+'" target="_blank" title="Otwórz w nowym oknie API reader">(Przeglądaj)</a>')
+                    }
+                    jQuery(this).html(tdHtml);
                 }
+                
+            });    
+        }
+        if (window.location.search.indexOf('page=tickets')){
+            uniSettings = GM_getValue('uniSettings');
+            if (uniSettings == undefined){
+                $('body').append('<a target=\'_blank\' href=\'https://ogamepl.comastuff.com/\'>Przejdź na stronę Teamu, aby załadować operatorów.</a> Następnie odśwież tą stronę.')
             }
-        });
+            //console.log(uniSettings);
+
+            $('table table table tr:gt(2)').each(function(){
+                let currentServerText = $(this).find('td:eq(2)').text();
+                if (currentServerText != '00'){
+                    let go = uniSettings.find(x=>x.key==currentServerText).value;
+                    if (go.length>0){
+                        $(this).find('td:eq(1)').append(' ('+go+')')
+                    }
+                }
+            });
+        }
+
     }
 })();
