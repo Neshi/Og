b92a36401a9ed6d42f83de320d68503978215c14
Dawid Jachnik <djachnik@reference.be>
2022-04-11 23:36:36 +0200

    improved script

--- scripts/BashHelper.user.js
+++ scripts/BashHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bash Helper (beta)
 // @namespace    https://comastuff.com/
-// @version      1.1
+// @version      2.0
 // @description  Skrypt ten sprawdza czy została naruszona zasada Bash
 // @author       Neshi
 // @match        https://*.ogame.gameforge.com/game/admin2/flottenlog.php?uid=*&touser=*
@@ -23,18 +23,42 @@ function subtractTimeFromDate(objDate, intHours) {
 }
 
 function CountBash(){
+
+    let missionTypeRegex = /^[^\(]*/gm
     var attackTimeTable = [];
     $('.contbox table').each(function() {
         var firstRow = $(this).find('tr:first td.no:first-child').text();
 
-        if (firstRow.indexOf('Napadaj') < 0) {
+        if (firstRow.indexOf('Napadaj') < 0 && firstRow.indexOf('Niszcz') < 0) {
             return;
         }
         var coordsString = $(this).find('tr:eq(1) td:eq(2)').text();
+        var planetLink = $(this).find('tr:eq(1) td:eq(2) a').attr('href');
         var dateString = $(this).find('tr:eq(3) td:last').text();
-        attackTimeTable.push({text:dateString,time:new Date(dateString),coords:coordsString})
-    //console.log(coordsString);
+        var params = new Proxy(new URLSearchParams(planetLink), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        });
+
+        var returnTable = $(this).next();
+        var nextRow = returnTable.find('tr:first td.no:first-child').text();
+
+        if (nextRow.indexOf('Powrót na planetę') < 0){
+            return;
+        }
+        var backTimeString = returnTable.find('tr:eq(3) td:first').text();
+
+        console.log("Czas dolotu: ", dateString,"; Czas powrotu: ",backTimeString);
+        if (dateString === backTimeString){
+            let match = firstRow.match(missionTypeRegex)
+            var planetId = params.toplanet == null ? 'Zniszczony': params.toplanet;
+            attackTimeTable.push({text:dateString+' ('+match[0].replace(' ','')+')',time:new Date(dateString),coords:coordsString + ' ('+planetId+')'})
+        } else {
+            console.log("Zawrócone");
+        }
+
     });
+
+
 console.log('attacktimetable',attackTimeTable);
     var allCoords = [...new Set(attackTimeTable.map(item=>item.coords))];
     //console.log(allCoords);
@@ -74,6 +98,6 @@ console.log('attacktimetable',attackTimeTable);
 }
 
 
-$('.contbox .content h3:first').append('<button class="countBash" type="button" style="margin-left:10px;" >Sprawdź bash! (beta)</button>')
+$('.contbox .content h3:first').append('<button class="countBash" type="button" style="margin-left:10px;" >Sprawdź bash!</button>')
 
 $('.countBash').click(function(){ CountBash(); });
