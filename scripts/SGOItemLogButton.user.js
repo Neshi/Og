02844646633af5cf90c7abe504b575ce59fd7776
Dawid Jachnik <djachnik@outlook.com>
2022-05-25 22:21:47 +0200

    added real icon to itemlog button

--- scripts/SGOItemLogButton.user.js
+++ scripts/SGOItemLogButton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SGO+ Item log button
 // @namespace    https://comastuff.com/
-// @version      0.1
+// @version      0.2
 // @description  Skrypt ten dodaje przycisk do wykorzystanych przedmiotów na profilu gracza
 // @author       Neshi
 // @match        https://*.ogame.gameforge.com/game/admin2/*
@@ -16,7 +16,7 @@
         var clonedButton = $('img[alt="Dodaj przedmiot"]').parent().clone();
         clonedButton.attr('href',clonedButton.attr('href').replace('itemGive.php','itemlog.php'));
         clonedButton.find('img').attr('alt','Wykorzystane przedmioty');
-        clonedButton.find('img').css('backgroundColor','blue');
+        clonedButton.find('img').attr('src','https://gf2.geo.gfsrv.net/cdn15/8d2be0b35e00e50739350d85caf46b.gif');
         clonedButton.attr('onmouseover','javascript:tiptool(\'Wykorzystane przedmioty\');');
         $('img[alt="Dodaj przedmiot"]').parent().after(clonedButton);
     }
